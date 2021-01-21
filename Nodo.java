7653ca531905bbab95a577acc7e8f33460199b59
Iris153 <71736194+Iris153@users.noreply.github.com>
2021-01-21 17:48:08 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,20 @@
+package regu4;
+
+/**
+ *
+ * @author irisv
+ */
+public class Nodo {
+    public Nodo izquierda;
+	public Nodo derecha;
+	public int elemento; 
+	public int nivel;
+				 
+	public Nodo(int elemento) {
+		this.elemento = elemento;
+		this.izquierda = null;
+		this.derecha = null;
+		this.nivel = 0;
+	}
+    
+}
